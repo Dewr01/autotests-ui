f315521e3f473f3b138db33ff4126527edabee3f
Dewr01 <pivont@mail.ru>
2025-07-22 08:28:38 +0300

    Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,9 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import Page, Playwright
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page() -> Page:  # Аннотируем возвращаемое фикстурой значение
-    # Ниже идет инициализация и открытие новой страницы
-    with sync_playwright() as playwright:
-        # Запускаем браузер
-        browser = playwright.chromium.launch(headless=False)
-
-        # Передаем страницу для использования в тесте
-        yield browser.new_page()
-
-        # Закрываем браузер после выполнения тестов
-        browser.close()
+@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
