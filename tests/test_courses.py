9fafe78899032a693dd9494df66d6eaf041023c9
Dewr01 <pivont@mail.ru>
2025-07-24 18:14:38 +0300

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -9,37 +9,21 @@ from pages.create_course_page import CreateCoursePage
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-    # Открыть страницу
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
-    # Проверить заголовок и состояние кнопки создания
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-
-    # Проверить блоки изображений
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
-
-    # Проверить форму по умолчанию
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
-        title="",
-        estimated_time="",
-        description="",
-        max_score="0",
-        min_score="0"
+        title="", estimated_time="", description="", max_score="0", min_score="0"
     )
 
-    # Проверить раздел упражнений
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
 
-    # Загрузить изображение и проверить состояние загрузки
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
-    create_course_page.check_visible_preview_image()
-
-    # Заполнить форму курса
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright",
         max_score="100",
@@ -48,15 +32,10 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         estimated_time="2 weeks"
     )
 
-    # Создать курс
     create_course_page.click_create_course_button()
 
-    # Проверить страницу списка курсов
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-
-    # Проверить карточку созданного курса
-    courses_list_page.check_visible_course_card(
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
         index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
     )
 
@@ -69,6 +48,5 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible("username")
     courses_list_page.sidebar.check_visible()
 
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
