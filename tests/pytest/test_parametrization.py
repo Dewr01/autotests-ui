011af74c8bafa6a0c24d1b65f399bbbcfff1ae54
Dewr01 <pivont@mail.ru>
2025-07-27 19:31:42 +0300

    Знакомство с Allure

--- tests/pytest/test_parametrization.py
+++ tests/pytest/test_parametrization.py
@@ -2,9 +2,9 @@ import pytest
 from _pytest.fixtures import SubRequest
 
 
-@pytest.mark.parametrize("number", [1, 2, 3, -1])
-def test_numbers(number: int):
-    assert number > 0
+# @pytest.mark.parametrize("number", [1, 2, 3, -1])
+# def test_numbers(number: int):
+#     assert number > 0
 
 
 @pytest.mark.parametrize("number, expected", [(1, 1), (2, 4), (3, 9)])
