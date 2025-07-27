e60ce345e6d43a6df12cd1a54affde82152c85bc
Dewr01 <pivont@mail.ru>
2025-07-27 18:04:00 +0300

    Наращивание тестовой базы и рефакторинг

--- components/base_component.py
+++ components/base_component.py
@@ -1,6 +1,4 @@
-# base_component.py
 from typing import Pattern
-
 from playwright.sync_api import Page, expect
 
 
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,5 @@
-# base_page.py
-from playwright.sync_api import Page
+from typing import Pattern
+from playwright.sync_api import Page, expect
 
 
 class BasePage:
@@ -9,5 +9,9 @@ class BasePage:
     def visit(self, url: str):
         self.page.goto(url, wait_until='networkidle')
 
-    def reload(self):  # Метод для перезагрузки страницы
+    def reload(self):
         self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
+
